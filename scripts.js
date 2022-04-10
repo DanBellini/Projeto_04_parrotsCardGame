1f28a4f1eb3d1a3c8bd3d044820efafe7fe8dd15
DanielBellini <danielbellinijf21@gmail.com>
2022-04-10 09:14:24 -0300

    função de virar as cartas

--- scripts.js
+++ scripts.js
@@ -1,80 +1,97 @@
+let cards;
 
-    let cards
-let card1 = {
-    imagem: `<img src="./imagens/bobrossparrot.gif">`
-};
-let card2 = {
-    imagem: `<img src="./imagens/explodyparrot.gif">`
-};
-let card3 = {
-    imagem: `<img src="./imagens/fiestaparrot.gif">`
-};
-let card4 = {
-    imagem: `<img src="./imagens/metalparrot.gif">`
-};
-let card5 = {
-    imagem: `<img src="./imagens/revertitparrot.gif">`
-};
-let card6 = {
-    imagem: `<img src="./imagens/tripletsparrot.gif">`
-};
-let card7 = {
-    imagem: `<img src="./imagens/unicornparrot.gif">`
-};
+let library = [
+  "./imagens/bobrossparrot.gif",
+  "./imagens/explodyparrot.gif",
+  "./imagens/fiestaparrot.gif",
+  "./imagens/metalparrot.gif",
+  "./imagens/revertitparrot.gif",
+  "./imagens/tripletsparrot.gif",
+  "./imagens/unicornparrot.gif"
+];
 
-const library = [card1, card2, card3, card4, card5, card6, card7];
+let cardsrandom = [];
+let cardflip = [];
 
-let cardsrandom = []
+howManyCards();
 
+cardsInGame();
 
+randomSequence();
 
-howManyCards()
-
-cardsInGame()
-
-randomSequence()
-
-function howManyCards (){
-    cards = Number(prompt("Com quantas cartas vocês quer jogar? Escolha um numero par, entre 4 e 14"));
-    while (cards > 14 || cards < 4 || cards % 2 !== 0 || cards == NaN){
-        cards = Number(prompt("Com quantas cartas vocês quer jogar? Escolha um numero par, entre 4 e 14"));
-    }
+function howManyCards() {
+  cards = Number(
+    prompt(
+      "Com quantas cartas vocês quer jogar? \nEscolha um numero par, entre 4 e 14"
+    )
+  );
+  while (cards > 14 || cards < 4 || cards % 2 !== 0 || cards == NaN) {
+    cards = Number(
+      prompt(
+        "Por favor, escolha com cartas vocês quer jogar? \nEscolha um numero par, entre 4 e 14"
+      )
+    );
+  }
 }
 
-function cardsInGame (){
-    let i = 0
-    cards = cards/2 -1
-    while(i <= cards){
+function cardsInGame() {
+  let i = 0;
+  cards = cards / 2 - 1;
+  while (i <= cards) {
+    let bodyCard = `
+            <div onclick="cardSelect(this)" class="card">
+                    <img class="parrot hidden" src="${library[i]}">
+            </div>`;
 
-        let bodyCard =`
-        <div class="card">
-            <div class="front-face face">
-                <img src="./imagens/front.png">
-            </div>
-            <div class="back-face face">
-                ${library[i].imagem}
-            </div>
-        </div>`;
-
-    cardsrandom.push(bodyCard)
-    cardsrandom.push(bodyCard)
-    i++
-    }
+    cardsrandom.push(bodyCard);
+    cardsrandom.push(bodyCard);
+    i++;
+  }
 }
 
-function randomSequence (){
-    cardsrandom.sort(comparador)
-    let i =0
-    console.log(cards)
-    cards = cards*2 + 1;
-    while (i <= cards){
-        inGame = document.querySelector(".game")
-        inGame.innerHTML = inGame.innerHTML + cardsrandom[i]
-        i++
-    }
-    
+function randomSequence() {
+  cardsrandom.sort(comparador);
+  let i = 0;
+  cards = cards * 2 + 1;
+  while (i <= cards) {
+    let inGame = document.querySelector(".game");
+    inGame.innerHTML = inGame.innerHTML + cardsrandom[i];
+    i++;
+  }
 }
 
-function comparador() { 
-        return Math.random() - 0.5; 
+function comparador() {
+  return Math.random() - 0.5;
+}
+
+function cardSelect(element) {
+  cardflip.push(element);
+
+  if (cardflip.length < 3) {
+    element.classList.add("flip");
+    element.querySelector(".parrot").classList.remove("hidden");
+
+    if (cardflip.length === 2) {
+        let firstCard = cardflip[0];
+        let secondCard = cardflip[1];
+        let value1 = firstCard.querySelector(".parrot").getAttribute("src");
+        let value2 = secondCard.querySelector(".parrot").getAttribute("src");
+        
+        let equals = value1 === value2
+
+        if (equals === true) {
+        cardflip = [];
+        } else if (equals === false){
+            setTimeout(function () {
+                firstCard.classList.remove("flip");
+                firstCard.querySelector(".parrot").classList.add("hidden");
+
+                secondCard.classList.remove("flip");
+                secondCard.querySelector(".parrot").classList.add("hidden");
+
+                cardflip = [];
+            }, 1000);
+        }
+    }
+  }
 }
